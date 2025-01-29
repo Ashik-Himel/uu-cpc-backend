1e879b39ce6ca1f913eb99bd4d43632d59d0f47f
Ashik-Himel <ashikujjamanhimel@gmail.com>
2025-01-29 14:57:13 +0600

    domain added in the cookie config

--- controllers/authController.js
+++ controllers/authController.js
@@ -39,6 +39,7 @@ export const register = async (req, res, next) => {
         secure: process.env.NODE_ENV === 'production',
         sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
         maxAge: 7 * 24 * 60 * 60 * 1000,
+        domain: process.env.NODE_ENV === 'production' ? 'uucpc.vercel.app' : 'localhost',
       })
       .status(201)
       .json({
@@ -75,6 +76,7 @@ export const login = async (req, res, next) => {
         secure: process.env.NODE_ENV === 'production',
         sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
         maxAge: 7 * 24 * 60 * 60 * 1000,
+        domain: process.env.NODE_ENV === 'production' ? 'uucpc.vercel.app' : 'localhost',
       })
       .status(200)
       .json({
